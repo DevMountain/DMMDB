53a021417558dd7f9b5251c5f7a33b40cf1cf3a1
Taylor Mott <trm36@humboldt.edu>
2015-05-22 10:56:22 -0600

    Removes Storyboard
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- MovieSearch2/AppDelegate.m
+++ MovieSearch2/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MovieListViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[MovieListViewController new]];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
