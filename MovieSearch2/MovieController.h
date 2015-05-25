1e2e4d4d8c05bd32247db350d65108cb92794f90
Taylor Mott <trm36@humboldt.edu>
2015-05-25 10:34:49 -0600

    Rebrands App With DevMountain Name
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- MovieSearch2/MovieController.h
+++ MovieSearch2/MovieController.h
@@ -3,7 +3,7 @@
 //  MovieSearch2
 //
 //  Created by Taylor Mott on 22 May 15.
-//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
