b6e74c426516a6f8394461b5be4726ccb869adec
Taylor Mott <trm36@humboldt.edu>
2015-05-25 15:48:23 -0600

    Cleans up project, uses @import, devmountain brand
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- MovieSearch2/Movie.h
+++ MovieSearch2/Movie.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface Movie : NSObject
 
--- MovieSearch2/MovieListTableViewDataSource.h
+++ MovieSearch2/MovieListTableViewDataSource.h
@@ -3,7 +3,7 @@
 //  MovieSearch2
 //
 //  Created by Taylor Mott on 25 May 15.
-//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
 @import Foundation;
